f88a34cd4875089d6097a03cce11a7b4c8a36971
Kacper Doga <doga.kacper@gmail.com>
2023-12-28 21:46:15 +0100

    image upload controller

--- src/controllers.php
+++ src/controllers.php
@@ -1,6 +1,9 @@
 <?php
 require_once 'business.php';
 
+define("IMAGE_MAX_SIZE", pow(2, 20));
+define("MB_SIZE", pow(2, 20));
+
 function gallery(&$model) {
     $page = $_GET['page'] ?? 1;
 
@@ -12,4 +15,43 @@ function gallery(&$model) {
     $model["images"] = get_images_from_page($page);
 
     return 'gallery_view';
+}
+
+function upload_image(&$model) {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $validation = '';
+
+        if (empty($_FILES['uploadImage'])) {
+            $validation .= "Image have not been sent.<br>";
+            $model['validation'] = $validation;
+            return 'upload_view';
+        }
+        $extension = pathinfo($_FILES['uploadImage']['name'], PATHINFO_EXTENSION);
+
+        if ($extension != "jpg" && $extension != "png")
+            $validation .= "Image has to have JPG or PNG extension.<br>";
+        if ($_FILES['uploadImage']['size'] > IMAGE_MAX_SIZE)
+            $validation .= "Max file size " . IMAGE_MAX_SIZE * 1.0 / MB_SIZE . "MB<br>";
+
+        if ($validation !== '') {
+            $model['validation'] = $validation;
+            return 'upload_view';
+        }
+
+        do {
+            $name = uniqid('image_');
+        } while (file_exists(IMAGE_DIR . "/" . $name));
+
+        $target = IMAGE_DIR . "/" . $name . "." . $extension;
+
+        if (!move_uploaded_file($_FILES['uploadImage']['tmp_name'], $target)) {
+            $validation = "Upload not completed, unknown error occurred.";
+            $model['validation'] = $validation;
+            return 'upload_view';
+        }
+
+        return 'redirect:/';
+    }
+
+    return 'upload_view';
 }
