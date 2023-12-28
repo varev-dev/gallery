ffab533823569e21d153fdb3943dd858e08ca5a3
Kacper Doga <doga.kacper@gmail.com>
2023-12-28 16:35:40 +0100

    list of image names, images on page, pages number

---
+++ src/business.php
@@ -0,0 +1,36 @@
+<?php
+const IMAGE_DIR = "images";
+const IMAGES_PER_PAGE = 12;
+
+function get_last_page_number() {
+    $images = get_images();
+    return count($images) / IMAGES_PER_PAGE;
+}
+
+function get_images_from_page(int $page) {
+    $images = get_images();
+
+    $offset = ($page - 1) * IMAGES_PER_PAGE;
+    $images = array_slice($images, $offset, IMAGES_PER_PAGE);
+
+    return $images;
+}
+
+function get_images(): array {
+    $images = [];
+    $files = scandir(IMAGE_DIR);
+
+    if (!$files)
+        return [];
+
+    foreach ($files as $file) {
+        $extension = pathinfo($file, PATHINFO_EXTENSION);
+
+        if ($extension != "jpg" && $extension != "png")
+            continue;
+
+        $images[] = IMAGE_DIR . "/" . $file;
+    }
+
+    return $images;
+}
