b1fd4853a3ef2ec23753be449c79e766fdaea85c
Kacper Doga <doga.kacper@gmail.com>
2024-01-01 22:54:31 +0100

    create sanitize, validate, escape functions for register data

--- src/controllers_utils.php
+++ src/controllers_utils.php
@@ -42,3 +42,50 @@ function check_if_no_special_chars(string $text): bool {
 
     return true;
 }
+
+function check_is_register_form_set(array $data): string {
+    $output = "";
+
+    if (!isset($data['login']))
+        $output .= 'Login was not set.<br>';
+    if (!isset($data['email']))
+        $output .= 'email was not set.<br>';
+    if (!isset($data['pwd']))
+        $output .= 'Password was not set.<br>';
+    if (!isset($data['pwd_rp']))
+        $output .= 'Repeat password was not set.<br>';
+
+    return $output;
+}
+
+function sanitize_register_data(array &$data) {
+    $data['login'] = filter_var($data['login'], FILTER_SANITIZE_STRING);
+    $data['email'] = filter_var($data['email'], FILTER_SANITIZE_EMAIL);
+    $data['pwd'] = filter_var($data['pwd'], FILTER_SANITIZE_STRING);
+}
+
+function validate_register_form(array $data, string $pwd_rp): string {
+    $output = "";
+
+    if (!match_to_pattern($data['login'], '[\w]', MIN_LOGIN_LENGTH))
+        $output .= "Login is not valid.<br>";
+    if (!filter_var($data['email'], FILTER_VALIDATE_EMAIL))
+        $output .= "Given email is not valid.<br>";
+    if (!match_to_pattern($data['pwd'], '[\s\S]', MIN_PASSWORD_LENGTH))
+        $output .= "Password is not valid.<br>";
+    if ($data['pwd'] != $pwd_rp)
+        $output .= "Password and repeated password are not the same.<br>";
+
+    return $output;
+}
+
+function match_to_pattern(string $text, string $pattern, int $min_length, int $max_length = 128) {
+    $pattern = "/^$pattern{{$min_length},$max_length}$/";
+    return preg_match($pattern, $text);
+}
+
+function escape_user_data(array &$user) {
+    $user['login'] = htmlentities($user['login'], ENT_QUOTES, 'UTF-8');
+    $user['email'] = htmlentities($user['email'], ENT_QUOTES, 'UTF-8');
+    $user['pwd'] = htmlentities($user['pwd'], ENT_QUOTES, 'UTF-8');
+}
