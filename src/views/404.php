1730ff08f71e57d7228fdb0e76d377a25847eb05
Kacper Doga <doga.kacper@gmail.com>
2024-01-02 22:53:02 +0100

    404 view

---
+++ src/views/404.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Gallery</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="static/css/styles.css"/>
+</head>
+<body>
+<header>
+    <div>
+        <h1 class="title_bar">404 Not Found</h1>
+    </div>
+    <hr>
+</header>
+<section style="display: flex; flex-direction: column; justify-content: center; align-items: center">
+    <a href="/" class="cancel">Return</a>
+    <img src="images/404.svg" alt="404 image" style="max-width: 80vw; max-height: 60vh"/>
+</section>
+<?php include "includes/footer.inc.php" ?>
+</body>
+</html>
