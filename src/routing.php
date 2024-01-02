2ed130be736a5d418128e0e18cb1f954634d1c40
Kacper Doga <doga.kacper@gmail.com>
2024-01-02 16:21:51 +0100

    endpoints for saving/removing/view images saved in session

--- src/routing.php
+++ src/routing.php
@@ -3,6 +3,9 @@
 $routing = [
     '/' => 'gallery',
     '/gallery' => 'gallery',
+    '/save_images' => 'save_images',
+    '/remove_images' => 'remove_images',
+    '/saved' => 'saved',
     '/upload' => 'upload_image',
     '/register' => 'register',
     '/login' => 'login',
